c3d8da79d3f9ebe737a83b15e66078ca63546a4d
amila1998 <amiladevin@gmail.com>
2023-03-19 03:21:37 +0530

    User Profile , Bottom navigation , Login , register

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:hardwarehub/Screens/Admin/AdminScreen.dart';
-import 'package:hardwarehub/Screens/Auth/Home.dart';
+import 'package:hardwarehub/PageTranstitons/MyPageTransitionsTheme.dart';
+import 'package:hardwarehub/Providers/ItemProvider.dart';
+import 'package:hardwarehub/Screens/Auth/AuthScreen.dart';
 import 'package:hardwarehub/Screens/Auth/LoginScreen.dart';
-import 'package:hardwarehub/Screens/User/UserScreen.dart';
+import 'package:hardwarehub/Screens/HomeScreen.dart';
 import 'package:hardwarehub/firebase_options.dart';
+import 'package:provider/provider.dart';
 
 
 void main() async {
@@ -22,47 +23,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, AsyncSnapshot<User?> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const CircularProgressIndicator();
+        } else if (snapshot.hasData) {
+          return ChangeNotifierProvider(
+            create: (_) => ItemProvider(),
+            child: const MaterialApp(
+              title: 'Hardware Hub',
+              home: HomeScreen(),
+            ),
+          );
+        } else {
+          return  MaterialApp(
+            title: 'Hardware Hub',
+            theme: ThemeData(
+        pageTransitionsTheme: const PageTransitionsTheme(
+          builders: {
+            TargetPlatform.iOS: MyPageTransitionsTheme(),
+            TargetPlatform.android: MyPageTransitionsTheme(),
+            TargetPlatform.fuchsia: MyPageTransitionsTheme(),
+            TargetPlatform.windows:MyPageTransitionsTheme(),
+          },
+        ),
       ),
-      home: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (snapshot.hasData) {
-            final User user = snapshot.data!;
-            return FutureBuilder(
-              future: FirebaseFirestore.instance
-                  .collection('users')
-                  .doc(user.uid)
-                  .get(),
-              builder: (BuildContext context,
-                  AsyncSnapshot<DocumentSnapshot> snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                final Map<String, dynamic>? data = snapshot.data?.data() as Map<String, dynamic>?;
-                final String role = data?['role'] ?? '';
-                return role == 'Buyer'
-                    ? AdminScreen(user: user)
-                    : UserScreen(user: user);
-              },
-            );
-          }
-          return const LoginScreen();
-        },
-      ),
-      routes: {
-        '/login': (BuildContext context) => const LoginScreen(),
-        '/home': (BuildContext context) => const MyApp(),
+            home: const AuthScreen(),
+          );
+        }
       },
     );
   }
